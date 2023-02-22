537d696aaa67db4bd9fa84f47439a00ccdc0e05c
Harry3795 <pradeepkumar20495@gmail.com>
2023-02-22 10:55:14 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap'
+import React from 'react';
+import Head1 from './components/header1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <Head1 name="first"  pradeep="hello harry"/>
+   <h1>hello pradeep</h1>
+   <Button type="submit" color='primary' >Submit</Button>
+   <Head1 name="second" pradeep="hello harry"/>
+   <Head1 name="third" pradeep="hello harry"/>
     </div>
   );
 }
---
+++ src/components/header1.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import { Header1 } from "react";
+function Head1({name,pradeep}) {
+    return(
+
+        <div style={{background:'cyan'}}>
+            <h1> {name} </h1>
+            <h2>{pradeep}</h2>
+        </div>
+    )
+}
+export default Head1;
